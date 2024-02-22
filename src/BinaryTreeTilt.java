0b5073a21c2fb37505ec67e6e48ea9e62965b893
Prameya Kirtikar <prameya.kirtikar@gmail.com>
2024-02-21 22:49:48 -0800

    Deleted Goolge interview prep. Fixed binary Tilt and Angry Owner

--- src/BinaryTreeTilt.java
+++ src/BinaryTreeTilt.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class BinaryTreeTilt {
+public class BinaryTreeTilt
+{
+    int res;
+    public int findTilt(TreeNode root)
+    {
+        res=0;
+        if(root==null)
+            return 0;
+        helper(root);
+        return res;
+    }
+
+    public int helper(TreeNode root)
+    {
+        if(root==null)
+            return 0;
+        int l=helper(root.left);
+        int r=helper(root.right);
+        res+=Math.abs(l-r);
+        return l+r+root.val;
+    }
 }
--- src/PaintFence.java
+++ src/PaintFence.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class PaintFence {
+public class PaintFence
+{
+    /*
+        You are painting a fence of n posts with k different colors. You must paint the posts following these rules:
+        Every post must be painted exactly one color.
+        There cannot be three or more consecutive posts with the same color.
+        Given the two integers n and k, return the number of ways you can paint the fence.
+
+        Example 1:
+        Input: n = 3, k = 2
+        Output: 6
+        Explanation: All the possibilities are shown.
+        Note that painting all the posts red or all the posts green is invalid because there cannot be three posts in a row with the same color.
+
+        Example 2:
+        Input: n = 1, k = 1
+        Output: 1
+
+        Example 3:
+        Input: n = 7, k = 2
+        Output: 42
+
+
+        Constraints:
+        1 <= n <= 50
+        1 <= k <= 105
+        The testcases are generated such that the answer is in the range [0, 231 - 1] for the given n and k.
+     */
+    public int numWays(int n, int k)
+    {
+        Integer[] memo=new Integer[n+1];
+        return helper(n,k,memo);
+    }
+
+    public int helper(int n, int k, Integer[] memo)
+    {
+        if(n==1)
+            return k;
+        if(n==2)
+            return k*k;
+        if(memo[n]!=null)
+            return memo[n];
+
+        memo[n]=(k-1)*(helper(n-1,k,memo)+helper(n-2,k,memo));
+        return memo[n];
+    }
 }
