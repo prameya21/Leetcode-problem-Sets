2a4d510a1a01cd6fe776e6da877cf951003f2255
Prameya Kirtikar <prameya.kirtikar@gmail.com>
2024-02-23 21:32:58 -0800

    Fixed Brace Expansion and lowest common ancestor 4

--- src/BraceExpansion.java
+++ src/BraceExpansion.java
@@ -1,2 +1,91 @@
-package PACKAGE_NAME;public class BraceExpansion {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class BraceExpansion
+{
+    /*
+        You are given a string s representing a list of words. Each letter in the word has one or more options.
+        If there is one option, the letter is represented as is.
+        If there is more than one option, then curly braces delimit the options. For example, "{a,b,c}" represents options ["a", "b", "c"].
+        For example, if s = "a{b,c}", the first character is always 'a', but the second character can be 'b' or 'c'. The original list is ["ab", "ac"].
+
+        Return all words that can be formed in this manner, sorted in lexicographical order.
+
+        Example 1:
+        Input: s = "{a,b}c{d,e}f"
+        Output: ["acdf","acef","bcdf","bcef"]
+
+        Example 2:
+        Input: s = "abcd"
+        Output: ["abcd"]
+
+
+        Constraints:
+
+        1 <= s.length <= 50
+        s consists of curly brackets '{}', commas ',', and lowercase English letters.
+        s is guaranteed to be a valid input.
+        There are no nested curly brackets.
+        All characters inside a pair of consecutive opening and ending curly brackets are different.
+     */
+
+    public String[] expand(String s)
+    {
+        if(s==null)
+            return new String[0];
+        List<List<Character>> tmp=new ArrayList<>();
+        parse(s,tmp);
+        List<String> res=new ArrayList<>();
+        dfs(0,tmp,new StringBuilder(), res);
+        return res.toArray(new String[res.size()]);
+    }
+
+    public void dfs(int idx, List<List<Character>> tmp, StringBuilder sb, List<String> res)
+    {
+        if(idx==tmp.size())
+            res.add(sb.toString());
+        else
+        {
+            for(int i=0;i<tmp.get(idx).size();i++)
+            {
+                sb.append(tmp.get(idx).get(i));
+                dfs(idx+1, tmp,sb,res);
+                sb.setLength(sb.length()-1);
+            }
+        }
+    }
+
+    public void parse(String s, List<List<Character>> tmp)
+    {
+        for(int i=0;i<s.length();i++)
+        {
+            List<Character> lst=new ArrayList<>();
+            if(s.charAt(i)=='{')
+            {
+                while(s.charAt(i)!='}')
+                {
+                    if(Character.isAlphabetic(s.charAt(i)))
+                        lst.add(s.charAt(i));
+                    i++;
+                }
+                Collections.sort(lst);
+            }
+            else
+            {
+                lst.add(s.charAt(i));
+            }
+            tmp.add(lst);
+        }
+
+    }
+
+    public static void main(String[] args)
+    {
+        BraceExpansion obj=new BraceExpansion();
+        String[] res=obj.expand("{a,b,c}ghj{n,m}yt{op}");
+        for(String s:res)
+            System.out.println(s);
+    }
 }
--- src/LowestCommonAncestorIV.java
+++ src/LowestCommonAncestorIV.java
@@ -1,2 +1,78 @@
-package PACKAGE_NAME;public class LowestCommonAncestorIV {
+import java.util.HashSet;
+import java.util.Set;
+
+public class LowestCommonAncestorIV
+{
+    /*
+        Given the root of a binary tree and an array of TreeNode objects nodes, return the lowest common ancestor (LCA) of all the nodes in nodes. All the nodes will exist in the tree, and all values of the tree's nodes are unique.
+        Extending the definition of LCA on Wikipedia: "The lowest common ancestor of n nodes p1, p2, ..., pn in a binary tree T is the lowest node that has every pi as a descendant (where we allow a node to be a descendant of itself) for every valid i". A descendant of a node x is a node y that is on the path from node x to some leaf node.
+
+        Example 1:
+        Input: root = [3,5,1,6,2,0,8,null,null,7,4], nodes = [4,7]
+        Output: 2
+        Explanation: The lowest common ancestor of nodes 4 and 7 is node 2.
+
+        Example 2:
+        Input: root = [3,5,1,6,2,0,8,null,null,7,4], nodes = [1]
+        Output: 1
+        Explanation: The lowest common ancestor of a single node is the node itself.
+
+        Example 3:
+        Input: root = [3,5,1,6,2,0,8,null,null,7,4], nodes = [7,6,2,4]
+        Output: 5
+        Explanation: The lowest common ancestor of the nodes 7, 6, 2, and 4 is node 5.
+
+        Constraints:
+
+        The number of nodes in the tree is in the range [1, 104].
+        -109 <= Node.val <= 109
+        All Node.val are unique.
+        All nodes[i] will exist in the tree.
+        All nodes[i] are distinct.
+     */
+
+    TreeNode res;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode[] nodes)
+    {
+        res=null;
+        if(root==null)
+            return res;
+        Set<Integer> dict=new HashSet<>();
+        for(TreeNode n:nodes)
+            dict.add(n.val);
+        helper(root,dict);
+        return res;
+    }
+
+    public boolean helper(TreeNode root, Set<Integer> dict)
+    {
+        if(root==null)
+            return false;
+        boolean left=helper(root.left,dict);
+        boolean right=helper(root.right,dict);
+        if(left && right)
+            res=root;
+        else if(left || right)
+            res=left?root.left:root.right;
+        else if(dict.contains(root.val))
+            res=root;
+        return dict.contains(root.val);
+
+    }
+
+
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode[] nodes)
+    {
+        if(root==null)
+            return null;
+        for(TreeNode n:nodes)
+            if(root==n)
+                return root;
+        TreeNode left=lowestCommonAncestor2(root.left,nodes);
+        TreeNode right=lowestCommonAncestor2(root.right,nodes);
+        if(left!=null && right!=null)
+            return root;
+        else
+            return left==null?right:left;
+    }
 }
