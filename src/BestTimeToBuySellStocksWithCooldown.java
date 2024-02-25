06ed6a4bf00dfb47474326864cd58a8a9ad5b0b4
Prameya Kirtikar <prameya.kirtikar@gmail.com>
2024-02-24 18:29:59 -0800

    Fixed stock cooldwon and conversions

--- src/BestTimeToBuySellStocksWithCooldown.java
+++ src/BestTimeToBuySellStocksWithCooldown.java
@@ -1,2 +1,57 @@
-package PACKAGE_NAME;public class BestTimeToBuySellStocksWithCooldown {
+public class BestTimeToBuySellStocksWithCooldown
+{
+    /*
+        You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+        Find the maximum profit you can achieve. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times) with the following restrictions:
+
+        After you sell your stock, you cannot buy stock on the next day (i.e., cooldown one day).
+        Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+
+
+
+        Example 1:
+
+        Input: prices = [1,2,3,0,2]
+        Output: 3
+        Explanation: transactions = [buy, sell, cooldown, buy, sell]
+        Example 2:
+
+        Input: prices = [1]
+        Output: 0
+
+     */
+
+    public int maxProfit(int[] prices)
+    {
+        if(prices==null || prices.length==0)
+            return 0;
+        Integer[][] memo=new Integer[prices.length][3];
+        return helper(prices,memo,0,0);
+    }
+    public int helper(int[] prices, Integer[][] memo, int idx, int status)
+    {
+        if(idx>=prices.length)
+            return 0;
+        if(memo[idx][status]!=null)
+            return memo[idx][status];
+
+        int doNothing=helper(prices,memo,idx+1,status);
+        int doSomething;
+        if(status==0)
+            doSomething=helper(prices,memo,idx+1,1)-prices[idx];
+        else if(status==1)
+            doSomething=helper(prices,memo,idx+1,2)+prices[idx];
+        else
+            doSomething=helper(prices,memo,idx+1,0);
+        memo[idx][status]=Math.max(doSomething,doNothing);
+
+        return memo[idx][status];
+    }
+
+    public static void main(String[] args)
+    {
+        BestTimeToBuySellStocksWithCooldown obj=new BestTimeToBuySellStocksWithCooldown();
+        System.out.println(obj.maxProfit(new int[]{1,2,3,0,2}));
+    }
 }
--- src/ZigZagConversion.java
+++ src/ZigZagConversion.java
@@ -1,2 +1,70 @@
-package PACKAGE_NAME;public class ZigZagConversion {
+public class ZigZagConversion
+{
+    /*
+        The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+        P   A   H   N
+        A P L S I I G
+        Y   I   R
+        And then read line by line: "PAHNAPLSIIGYIR"
+
+        Write the code that will take a string and make this conversion given a number of rows:
+
+        string convert(string s, int numRows);
+
+
+        Example 1:
+
+        Input: s = "PAYPALISHIRING", numRows = 3
+        Output: "PAHNAPLSIIGYIR"
+        Example 2:
+
+        Input: s = "PAYPALISHIRING", numRows = 4
+        Output: "PINALSIGYAHRPI"
+        Explanation:
+        P     I    N
+        A   L S  I G
+        Y A   H R
+        P     I
+        Example 3:
+
+        Input: s = "A", numRows = 1
+        Output: "A"
+     */
+    public String convert(String s, int numRows)
+    {
+        if(numRows==1)
+            return s;
+        Character[][] arr=new Character[numRows][s.length()];
+        int i=0, j=0, k=0;
+        boolean flag=false;
+        while(k<s.length())
+        {
+            char c=s.charAt(k);
+            arr[i][j]=c;
+            if(i==numRows-1)
+                flag=true;
+            if(i==0)
+                flag=false;
+            if(flag)
+            {
+                i--;
+                j++;
+            }
+            else
+                i++;
+            k++;
+        }
+        StringBuilder sb=new StringBuilder();
+        for(i=0;i<arr.length;i++)
+        {
+            for(j=0;j<arr[0].length;j++)
+            {
+                if(arr[i][j]==null)
+                    continue;
+                sb.append(arr[i][j]);
+            }
+        }
+        return sb.toString();
+    }
 }
