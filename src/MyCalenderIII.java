c3ea2f4e8dbaa1862be089b1cee5149ba9f3b9e0
Prameya Kirtikar <prameya.kirtikar@gmail.com>
2024-02-25 18:45:36 -0800

    Fixed Calendar 3

--- src/MyCalenderIII.java
+++ src/MyCalenderIII.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class MyCalenderIII {
+import java.util.*;
+
+public class MyCalenderIII
+{
+    /*
+        A k-booking happens when k events have some non-empty intersection (i.e., there is some time that is common to all k events.)
+        You are given some events [startTime, endTime), after each given event, return an integer k representing the maximum k-booking between all the previous events.
+
+        Implement the MyCalendarThree class:
+        MyCalendarThree() Initializes the object.
+        int book(int startTime, int endTime) Returns an integer k representing the largest integer such that there exists a k-booking in the calendar.
+
+        Example 1:
+        Input
+        ["MyCalendarThree", "book", "book", "book", "book", "book", "book"]
+        [[], [10, 20], [50, 60], [10, 40], [5, 15], [5, 10], [25, 55]]
+        Output
+        [null, 1, 1, 2, 3, 3, 3]
+
+        Explanation
+        MyCalendarThree myCalendarThree = new MyCalendarThree();
+        myCalendarThree.book(10, 20); // return 1
+        myCalendarThree.book(50, 60); // return 1
+        myCalendarThree.book(10, 40); // return 2
+        myCalendarThree.book(5, 15); // return 3
+        myCalendarThree.book(5, 10); // return 3
+        myCalendarThree.book(25, 55); // return 3
+
+        Constraints:
+
+        0 <= startTime < endTime <= 109
+        At most 400 calls will be made to book.
+     */
+    class MyCalendarThree {
+        TreeMap<Integer,Integer> map;
+        public MyCalendarThree()
+        {
+            map=new TreeMap<>();
+        }
+
+        public int book(int start, int end)
+        {
+            map.put(start,map.getOrDefault(start,0)+1);
+            map.put(end,map.getOrDefault(end,0)-1);
+            int res=0,sum=0;
+            for(int i:map.values())
+            {
+                sum+=i;
+                res=Math.max(res,sum);
+            }
+            return res;
+
+        }
+    }
 }
