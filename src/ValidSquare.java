4ac9e9e10bd349b96c4bbbb15e384f74084f1a2e
Prameya Kirtikar <prameya.kirtikar@gmail.com>
2024-02-25 18:36:12 -0800

    Fixed ValidSquare

--- src/ValidSquare.java
+++ src/ValidSquare.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ValidSquare
 {
@@ -27,6 +29,17 @@ public class ValidSquare
         -104 <= xi, yi <= 104
      */
 
+    public boolean validSquare2(int[] p1, int[] p2, int[] p3, int[] p4)
+    {
+        Set<Integer> set=new HashSet<>();
+        set.add(distance(p1,p2));
+        set.add(distance(p1,p3));
+        set.add(distance(p1,p4));
+        set.add(distance(p2,p3));
+        set.add(distance(p2,p4));
+        set.add(distance(p3,p4));
+        return !set.contains(0) && set.size()==2;
+    }
     public boolean validSquare(int[] p1, int[] p2, int[] p3, int[] p4)
     {
         int[][] p={p4,p1,p2,p3};
@@ -44,6 +57,11 @@ public class ValidSquare
 
     }
 
+    public int distance(int[] p1, int[] p2)
+    {
+        // distance = (x1 - x2) ^ 2 + (y1 - y2) ^ 2;
+        return ((p1[0] - p2[0]) * (p1[0] - p2[0])) + ((p1[1] - p2[1]) * (p1[1] - p2[1]));
+    }
     public double dist(int[] a, int[] b)
     {
         return (a[0]-b[0])*(a[0]-b[0])+(a[1]-b[1])*(a[1]-b[1]);
