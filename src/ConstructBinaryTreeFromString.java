18eb1d205ce07b1df7c88cceeedcf6f5d86ba15e
Prameya Kirtikar <prameya.kirtikar@gmail.com>
2024-02-26 19:57:07 -0800

    Fixed string binary tree construction and min op reduction

--- src/ConstructBinaryTreeFromString.java
+++ src/ConstructBinaryTreeFromString.java
@@ -1,2 +1,60 @@
-package PACKAGE_NAME;public class ConstructBinaryTreeFromString {
+public class ConstructBinaryTreeFromString
+{
+    /*
+        You need to construct a binary tree from a string consisting of parenthesis and integers.
+        The whole input represents a binary tree. It contains an integer followed by zero, one or two pairs of parenthesis. The integer represents the root's value and a pair of parenthesis contains a child binary tree with the same structure.
+        You always start to construct the left child node of the parent first if it exists.
+
+        Example 1:
+        Input: s = "4(2(3)(1))(6(5))"
+        Output: [4,2,6,3,1,5]
+
+        Example 2:
+        Input: s = "4(2(3)(1))(6(5)(7))"
+        Output: [4,2,6,3,1,5,7]
+
+        Example 3:
+        Input: s = "-4(2(3)(1))(6(5)(7))"
+        Output: [-4,2,6,3,1,5,7]
+
+
+        Constraints:
+
+        0 <= s.length <= 3 * 104
+        s consists of digits, '(', ')', and '-' only.
+     */
+
+    public TreeNode str2tree(String s)
+    {
+        if(s==null || s.length()==0)
+            return null;
+        int fp=s.indexOf('(');
+        int val=fp==-1?Integer.parseInt(s):Integer.parseInt(s.substring(0,fp));
+        TreeNode root=new TreeNode(val);
+        if(fp==-1)
+            return root;
+        int score=0, i=fp;
+        for(;i<s.length();i++)
+        {
+            if(s.charAt(i)=='(')
+                score++;
+            else if(s.charAt(i)==')')
+                score--;
+            if(score==0)
+                break;
+        }
+        root.left=str2tree(s.substring(fp+1,i));
+        root.right=i+2<=s.length()-1?str2tree(s.substring(i+2,s.length()-1)):null;
+        return root;
+    }
+
+    public static void main(String[] args)
+    {
+        ConstructBinaryTreeFromString obj=new ConstructBinaryTreeFromString();
+        System.out.println(obj.str2tree("4(2(3)(1))(6(5))"));
+        System.out.println(obj.str2tree("4(2(3)(1))(6(5)(7))"));
+        System.out.println(obj.str2tree("-4(2(3)(1))(6(5)(7))"));
+    }
+
+
 }
--- src/MinOpsReduceXToZero.java
+++ src/MinOpsReduceXToZero.java
@@ -1,2 +1,57 @@
-package PACKAGE_NAME;public class MinOpsReduceXToZero {
+public class MinOpsReduceXToZero
+{
+    /*
+        You are given an integer array nums and an integer x. In one operation, you can either remove the leftmost or the rightmost element from the array nums and subtract its value from x. Note that this modifies the array for future operations.
+        Return the minimum number of operations to reduce x to exactly 0 if it is possible, otherwise, return -1.
+
+        Example 1:
+        Input: nums = [1,1,4,2,3], x = 5
+        Output: 2
+        Explanation: The optimal solution is to remove the last two elements to reduce x to zero.
+
+        Example 2:
+        Input: nums = [5,6,7,8,9], x = 4
+        Output: -1
+
+        Example 3:
+        Input: nums = [3,2,20,1,1,3], x = 10
+        Output: 5
+        Explanation: The optimal solution is to remove the last three elements and the first two elements (5 operations in total) to reduce x to zero.
+
+        Constraints:
+
+        1 <= nums.length <= 105
+        1 <= nums[i] <= 104
+        1 <= x <= 109
+     */
+
+    public int minOperations(int[] nums, int x)
+    {
+        if(nums==null || nums.length==0)
+            return -1;
+        int sum=0;
+        for(int i:nums)
+            sum+=i;
+        sum-=x;
+        int res=Integer.MIN_VALUE, l=0, r=0, curr=0;
+        while(r<nums.length)
+        {
+            curr+=nums[r];
+            while(curr>sum && l<nums.length)
+                curr-=nums[l++];
+
+            if(curr==sum)
+                res=Math.max(res,r-l+1);
+            r++;
+        }
+        return res==Integer.MIN_VALUE?-1:nums.length-res;
+    }
+
+    public static void main(String[] args)
+    {
+        MinOpsReduceXToZero obj=new MinOpsReduceXToZero();
+        System.out.println(obj.minOperations(new int[]{1,1,4,2,3}, 5));
+        System.out.println(obj.minOperations(new int[]{5,6,7,8,9}, 4));
+        System.out.println(obj.minOperations(new int[]{3,2,20,1,1,3}, 10));
+    }
 }
